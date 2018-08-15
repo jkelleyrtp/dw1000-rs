fbb3879973cd32430b7481e267b39569421739f8
Hanno Braun <hanno@braun-robotics.com>
2018-08-15 08:27:51 +0200

    Use timer API from nrf52-hal for blink example

--- examples/blink.rs
+++ examples/blink.rs
@@ -5,8 +5,8 @@
 #![no_std]
 
 
-#[macro_use]
-extern crate cortex_m_rt;
+#[macro_use] extern crate cortex_m_rt;
+#[macro_use] extern crate nb;
 
 extern crate dwm1001;
 
@@ -17,10 +17,11 @@ use dwm1001::{
     cortex_m_rt::ExceptionFrame,
     nrf52_hal::{
         prelude::*,
+        nrf52::Peripherals,
         gpio::GpioExt,
-        nrf52::{
-            self,
-            Peripherals,
+        timer::{
+            Timer,
+            TimerExt,
         },
     },
 };
@@ -29,57 +30,32 @@ use dwm1001::{
 entry!(main);
 
 fn main() -> ! {
-    let mut p = Peripherals::take().unwrap();
+    let p = Peripherals::take().unwrap();
 
     let mut p0_14 = p.P0
         .split()
         .p0_14
         .into_push_pull_output();
 
-    // Configure TIMER0
-    p.TIMER0.shorts.write(|w|
-        w
-            .compare0_clear().enabled()
-            .compare0_stop().enabled()
-    );
-    p.TIMER0.prescaler.write(|w|
-        unsafe { w.prescaler().bits(5) } // 1 MHz
-    );
-    p.TIMER0.bitmode.write(|w|
-        w.bitmode()._32bit()
-    );
+    let mut timer = p.TIMER0.constrain();
 
     loop {
         // Set P0.14 to LOW, thereby enabling the LED
         p0_14.set_low();
 
-        delay(&mut p.TIMER0, 20_000); // 20ms
+        delay(&mut timer, 20_000); // 20ms
 
         // Set P0.14 to HIGH, thereby disabling the LED
         p0_14.set_high();
 
-        delay(&mut p.TIMER0, 230_000); // 230ms
+        delay(&mut timer, 230_000); // 230ms
     }
 }
 
 
-fn delay(timer: &mut nrf52::TIMER0, cycles: u32) {
-    // Configure timer to trigger EVENTS_COMPARE on number of cycles reached
-    timer.cc[0].write(|w|
-        // The timer was set to 32 bits, so all values of `cycles` are valid.
-        unsafe { w.cc().bits(cycles) }
-    );
-
-    // Start timer
-    timer.tasks_start.write(|w|
-        unsafe { w.bits(1) }
-    );
-
-    // Wait for timer to reach the desired value
-    while timer.events_compare[0].read().bits() == 0 {}
-
-    // Reset the event
-    timer.events_compare[0].write(|w| w);
+fn delay<T>(timer: &mut Timer<T>, cycles: u32) where T: TimerExt {
+    timer.start(cycles);
+    block!(timer.wait());
 }
 
 
