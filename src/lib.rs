e42eb61dff203afd0067391ff5a9b6c29959a12b
Hanno Braun <hanno@braun-robotics.com>
2018-08-20 10:04:42 +0200

    Add basic documentation and configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Driver crate for the DW1000 UWB transceiver
+
+
+#![no_std]
+
+#![deny(missing_docs)]
+#![deny(warnings)]
