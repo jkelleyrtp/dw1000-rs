e6a727982ea08d72d94137af95b998f22cb866fd
Hanno Braun <hanno@braun-robotics.com>
2018-08-24 08:43:31 +0200

    Add register trait
    This trait contains enough info to make the read code generic.

--- src/lib.rs
+++ src/lib.rs
@@ -47,34 +47,65 @@ impl<SPI> DW1000<SPI> where SPI: SpimExt {
 
     /// Read the device identifier (DEV_ID)
     pub fn dev_id(&mut self) -> Result<DEV_ID, spim::Error> {
-        // Set up the transmit buffer
-        //
-        // It consists of only one byte for the transaction header. Since this
-        // is a read operation, there is no transaction body.
         let header =
-            (0 & 0x80) |  // read
-            (0 & 0x40) |  // no sub-index
-            (0 & 0x3f);   // index of DEV_ID register
+            (0          & 0x80) |  // read
+            (0          & 0x40) |  // no sub-index
+            (DEV_ID::ID & 0x3f);   // index of DEV_ID register
         let tx_buffer = [header];
 
-        // Set up the receive buffer
-        //
-        // SPI is a synchronous interface, so we're going to receive a byte for
-        // every one we send. That means in addition to the 4 bytes we actually
-        // expect, we need an additional one that we receive while we send the
-        // header.
-        let mut rx_buffer = [0u8; 5];
+        let mut r = DEV_ID::new();
 
-        self.spim.read(&mut self.chip_select, &tx_buffer, &mut rx_buffer)?;
+        self.spim.read(&mut self.chip_select, &tx_buffer, r.rx_buffer())?;
 
-        Ok(DEV_ID(rx_buffer))
+        Ok(r)
     }
+}
+
+
+/// Implemented for all registers
+///
+/// This trait is for internal use only. Users of this library should never need
+/// to implement this trait, nor use its associated items.
+///
+/// The DW1000 user manual, section 7.1, specifies what the values of those
+/// constant should be for each register.
+pub trait Register {
+    /// The register ID
+    const ID:  u8;
+
+    /// The lenght of the register
+    const LEN: usize;
+
+    /// Creates an instance of the register
+    fn new() -> Self;
+
+    /// Returns a mutable reference to the register's internal buffer
+    ///
+    /// SPI is a synchronous interface, which means a bytes is received for
+    /// every byte that is sent, even though the bytes we receive while sending
+    /// something end up being ignored. Still, we need room for those bytes in
+    /// the buffer, so the length of the buffer must be equal to the length of
+    /// the register plus the length of the transaction header.
+    fn rx_buffer(&mut self) -> &mut [u8];
 }
 
 
 /// Device identifier - includes device type and revision info
 #[allow(non_camel_case_types)]
 pub struct DEV_ID([u8; 5]);
+
+impl Register for DEV_ID {
+    const ID:  u8    = 0x00;
+    const LEN: usize = 4;
+
+    fn new() -> Self {
+        DEV_ID([0; 5])
+    }
+
+    fn rx_buffer(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
 
 impl DEV_ID {
     /// Register Identification Tag
