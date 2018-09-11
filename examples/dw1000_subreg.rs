bf1c2ca528bfebde623f299cc75e9bec904b7396
Hanno Braun <hanno@braun-robotics.com>
2018-09-11 09:53:58 +0200

    Add example that tests sub-registers

---
+++ examples/dw1000_subreg.rs
@@ -0,0 +1,69 @@
+//! Accesses a DW1000 sub-register and verifies that this worked
+
+
+#![no_main]
+#![no_std]
+
+
+#[macro_use] extern crate cortex_m_rt;
+
+extern crate cortex_m_semihosting;
+extern crate dwm1001;
+extern crate panic_semihosting;
+
+
+use core::fmt::Write;
+
+use cortex_m_semihosting::hio;
+
+use dwm1001::{
+    dw1000,
+    DWM1001,
+};
+
+
+entry!(main);
+
+fn main() -> ! {
+    // Initialize debug output
+    let mut stdout = hio::hstdout()
+        .expect("Failed to initialize debug output");
+
+    let mut dwm1001 = DWM1001::take().unwrap();
+
+    write!(stdout, "Writing...\n");
+
+    dwm1001.DW1000
+        .write::<dw1000::DRX_TUNE2, _>(|w|
+            // Careful, only specific values are allowed here.
+            w.value(0x311A002D)
+        )
+        .expect("Failed to write to register");
+
+    write!(stdout, "Reading...\n");
+
+    let drx_tune2 = dwm1001.DW1000.read::<dw1000::DRX_TUNE2>()
+        .expect("Failed to read from register");
+
+    assert_eq!(drx_tune2.value(), 0x311A002D);
+
+    write!(stdout, "Writing...\n");
+
+    dwm1001.DW1000
+        .write::<dw1000::DRX_TUNE2, _>(|w|
+            // Careful, only specific values are allowed here.
+            w.value(0x313B006B)
+        )
+        .expect("Failed to write to register");
+
+    write!(stdout, "Reading...\n");
+
+    let drx_tune2 = dwm1001.DW1000.read::<dw1000::DRX_TUNE2>()
+        .expect("Failed to read from register");
+
+    assert_eq!(drx_tune2.value(), 0x313B006B);
+
+    write!(stdout, "Success!\n");
+
+    loop {}
+}
