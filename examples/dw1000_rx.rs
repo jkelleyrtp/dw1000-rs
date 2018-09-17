140dcd9ab558358968bfc2cb5e75d35a456a0d7d
Hanno Braun <hanno@braun-robotics.com>
2018-09-17 12:11:02 +0200

    Add examples that send and receive data
    As the comment explains, the receive example isn't completely reliable
    yet, but still, this is a breakthrough!

---
+++ examples/dw1000_rx.rs
@@ -0,0 +1,193 @@
+//! Waits to receive data and signals status via LEDs
+//!
+//! Please note that, for unknown reasons, this example only receives data once,
+//! then needs to be re-flashed to receive data again. It's a start.
+
+
+#![no_main]
+#![no_std]
+
+
+#[macro_use] extern crate cortex_m_rt;
+#[macro_use] extern crate dwm1001;
+
+extern crate cortex_m_semihosting;
+extern crate nb;
+extern crate panic_semihosting;
+
+
+use dwm1001::{
+    prelude::*,
+    debug,
+    dw1000,
+    nrf52832_hal::Delay,
+    DWM1001,
+};
+
+
+
+
+
+entry!(main);
+
+fn main() -> ! {
+    debug::init();
+
+    let mut dwm1001 = DWM1001::take().unwrap();
+
+    let     clocks = dwm1001.CLOCK.constrain().freeze();
+    let mut delay  = Delay::new(dwm1001.SYST, clocks);
+
+    dwm1001.DW_RST.reset_dw1000(&mut delay);
+
+    // Configure timer
+    let mut timer = dwm1001.TIMER0.constrain();
+
+    'outer: loop {
+        print!("Configure...\n");
+
+        // Set PLLLDT bit in EC_CTRL. According to the documentation of the
+        // CLKPLL_LL bit in SYS_STATUS, this bit needs to be set to ensure the
+        // reliable operation of the CLKPLL_LL bit. Since I've seen that bit
+        // being set, I want to make sure I'm not just seeing crap.
+        dwm1001.DW1000
+            .modify::<dw1000::EC_CTRL, _>(|_, w|
+                w.pllldt(0b1)
+            )
+            .expect("Failed to modify register");
+
+        // Now that PLLLDT is set, clear all bits in SYS_STATUS that depend on
+        // it for reliable operation. After that is done, these bits should work
+        // reliably.
+        dwm1001.DW1000
+            .write::<dw1000::SYS_STATUS, _>(|w|
+                w
+                    .cplock(0b1)
+                    .clkpll_ll(0b1)
+            )
+            .expect("Failed to write to register");
+
+        // If we cared about MAC addresses, which we don't in this example, we'd
+        // have to enable frame filtering at this point. By default it's off,
+        // meaning we'll receive everything, no matter who it is addressed to.
+
+        // We're expecting a preamble length of `64`. Set PAC size to the
+        // recommended value for that preamble length, according to section
+        // 4.1.1. The value we're writing to DRX_TUNE2 here also depends on the
+        // PRF, which we expect to be 16 MHz.
+        dwm1001.DW1000
+            .write::<dw1000::DRX_TUNE2, _>(|w|
+                // PAC size 8, with 16 MHz PRF
+                w.value(0x311A002D)
+            )
+            .expect("Failed to write to register");
+
+        // If we were going to receive at 110 kbps, we'd need to set the RXM110K
+        // bit in the System Configuration register. We're expecting to receive
+        // at 850 kbps though, so the default is fine. See section 4.1.3 for a
+        // detailed explanation.
+
+        print!("Receive...\n");
+
+        dwm1001.DW1000
+            .modify::<dw1000::SYS_CTRL, _>(|_, w|
+                w.rxenab(0b1)
+            )
+            .expect("Failed to modify register");
+
+        // Set timer for timeout
+        timer.start(5_000_000);
+
+        // Wait until frame has been received
+        loop {
+            let sys_status = dwm1001.DW1000.read::<dw1000::SYS_STATUS>()
+                .expect("Failed to read from register");
+
+            // Check progress
+            if sys_status.rxprd() == 0b1 {
+                dwm1001.DW1000.write::<dw1000::SYS_STATUS, _>(|w| w.rxprd(0b1))
+                    .expect("Failed to reset flag");
+                print!("Preamble detected\n");
+            }
+            if sys_status.rxsfdd() == 0b1 {
+                dwm1001.DW1000.write::<dw1000::SYS_STATUS, _>(|w| w.rxsfdd(0b1))
+                    .expect("Failed to reset flag");
+                print!("SFD detected\n");
+            }
+            if sys_status.rxphd() == 0b1 {
+                dwm1001.DW1000.write::<dw1000::SYS_STATUS, _>(|w| w.rxphd(0b1))
+                    .expect("Failed to reset flag");
+                print!("PHY header detected\n");
+            }
+            if sys_status.rxdfr() == 0b1 {
+                dwm1001.DW1000.write::<dw1000::SYS_STATUS, _>(|w| w.rxdfr(0b1))
+                    .expect("Failed to reset flag");
+                print!("Data frame ready\n");
+
+                // Check for errors
+                if sys_status.rxfce() == 0b1 {
+                    print!("FCS error\n");
+                    continue 'outer;
+                }
+                if sys_status.rxfcg() != 0b1 {
+                    print!("FCS not good\n");
+                    continue 'outer;
+                }
+
+                assert_eq!(sys_status.rxfcg(), 0b1);
+                assert_eq!(sys_status.rxfce(), 0b0);
+
+                break;
+            }
+
+            // Check errors
+            if sys_status.rxphe() == 0b1 {
+                dwm1001.DW1000.write::<dw1000::SYS_STATUS, _>(|w| w.rxphe(0b1))
+                    .expect("Failed to reset flag");
+                print!("PHY header error\n");
+
+                continue 'outer;
+            }
+
+            if timer.wait() != Err(nb::Error::WouldBlock) {
+                print!("Timeout\n");
+                continue 'outer;
+            }
+        }
+
+        print!("Process...\n");
+
+        // Read received frame
+        let rx_finfo = dwm1001.DW1000.read::<dw1000::RX_FINFO>()
+            .expect("Failed to read from register");
+        let rx_buffer = dwm1001.DW1000.read::<dw1000::RX_BUFFER>()
+            .expect("Failed to read from register");
+
+        let len  = rx_finfo.rxflen() as usize;
+        let data = &rx_buffer.data()[0 .. len];
+
+        print!("Received data: {:?}\n", data);
+
+        let expected_data = b"ping";
+
+        // Received data should have length of expected data, plus 2-byte CRC
+        // checksum.
+        if len != expected_data.len() + 2 {
+            print!("Unexpected length: {}\n", len);
+            continue;
+        }
+
+        if data[0 .. len - 2] != expected_data[..] {
+            print!("Unexpected data");
+            continue;
+        }
+
+        // Signal that data was received
+        for _ in 0..20 {
+            dwm1001.leds.D10.enable();
+            delay.delay_ms(30u32);
+            dwm1001.leds.D10.disable();
+            delay.delay_ms(30u32);
+        }
+    }
+}
