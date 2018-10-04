da989e4756f2dac3309399c22f8508472d439c9e
Hanno Braun <hanno@braun-robotics.com>
2018-10-04 08:57:34 +0200

    Add example to test delayed transmission

---
+++ examples/dw1000_delayed_tx.rs
@@ -0,0 +1,49 @@
+//! Continually sends data using delayed transmission
+
+
+#![no_main]
+#![no_std]
+
+
+#[macro_use] extern crate cortex_m_rt;
+#[macro_use] extern crate dwm1001;
+#[macro_use] extern crate nb;
+
+extern crate panic_semihosting;
+
+
+use dwm1001::{
+    debug,
+    dw1000::mac,
+    DWM1001,
+};
+
+
+#[entry]
+fn main() -> ! {
+    debug::init();
+
+    let mut dwm1001 = DWM1001::take().unwrap();
+
+    loop {
+        let sys_time = dwm1001.DW1000.ll()
+            .sys_time()
+            .read()
+            .expect("Failed to read system time")
+            .value();
+
+        let delay   = 10 * 64_000_000; // ~10 ms
+        let tx_time = sys_time + delay;
+
+        let mut tx = dwm1001.DW1000
+            .send(b"ping", mac::Address::broadcast(), Some(tx_time))
+            .expect("Failed to start receiver");
+
+        print!("Sending... ");
+
+        block!(tx.wait())
+            .expect("Failed to send data");
+
+        print!("done\n");
+    }
+}
