1cc0f117fd14e3ceadfee47334eb499c93d5ef04
Tyler Neely <t@jujit.su>
2019-01-07 18:33:30 +0100

    Update uarte_heapless_string.rs

--- examples/uarte_heapless_string.rs
+++ examples/uarte_heapless_string.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 use cortex_m_rt::entry;
 use heapless::String as HString;
 use nb::block;
@@ -18,16 +20,24 @@ fn main() -> ! {
     let mut timer = dwm1001.TIMER0.constrain();
 
     let mut s: HString<heapless::consts::U64> = HString::new();
-    s.push_str("halp plz\r\n");
+    s.push_str("halp plz ");
+    let original_len = s.len();
 
-    loop {
+    for i in 0.. {
         dwm1001.leds.D12.enable();
         delay(&mut timer, 20_000); // 20ms
         dwm1001.leds.D12.disable();
         delay(&mut timer, 230_000); // 230ms
 
+        write!(s, "{}\r\n", i);
+
         dwm1001.uart.write(s.as_bytes()).unwrap();
+
+        s.truncate(original_len);
     }
+    
+    // convince the compiler this never terminates
+    loop {}
 }
 
 fn delay<T>(timer: &mut Timer<T>, cycles: u32)
