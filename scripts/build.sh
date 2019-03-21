73f032d3f3eba69d61643add263fb496c532878f
Hanno Braun <hanno@braun-robotics.com>
2019-03-21 16:43:46 +0100

    Update CI build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,10 @@
 
 export RUSTFLAGS="-D warnings"
 
-cargo test --verbose &&
-cargo doc
+# `.cargo/config` defaults us to the microcontroller's target triple. We need
+# to override this here, to run `cargo test`. You may need to adapt this,
+# depending on you platform.
+TARGET=x86_64-unknown-linux-gnu
+
+cargo test --verbose --target=$TARGET &&
+cargo doc --verbose --target=$TARGET
