058af66fa1756df09ce4e487082a0cd5fcde6b7d
Per <Per Lindgren>
2019-10-18 14:58:38 +0200

    lis2dh driver integration

--- dwm1001/examples/lis2dh12.rs
+++ dwm1001/examples/lis2dh12.rs
@@ -1,43 +1,77 @@
 //! Accesses the LIS2DH12 3-axis accelerometer
+//!
+//! To build:
+//! cargo build --example lis2dh12 --features "dev rt semihosting" --release
+//!
+//! To debug:
+//! openocd -f openocd_jlink.cfg
+//! arm-none-eabi-gdb target/thumbv7em-none-eabihf/release/examples/lis2dh12 -x debug.gdb
 
 #![no_main]
 #![no_std]
 
-
 extern crate panic_semihosting;
-
 
 use cortex_m_rt::entry;
 
-use dwm1001::{
-    debug,
-    DWM1001,
-    print,
-};
+use dwm1001::{debug, print, DWM1001};
 
+use lis2dh12::{self, Accelerometer};
 
 #[entry]
 fn main() -> ! {
     debug::init();
 
-    let mut dwm1001 = DWM1001::take().unwrap();
+    print!("start\n");
+
+    let dwm1001 = DWM1001::take().unwrap();
 
     // SDO/SA0 is connected to the supply voltage, so the least significant bit
     // is 1. See datasheet, section 6.1.1.
-    let address = 0b0011001;
 
-    let mut write_buf = [0u8; 1];
-    let mut read_buf  = [0u8; 1];
+    let address = lis2dh12::SlaveAddr::Alternative(true);
 
-    // Read WHOAMI register (address 0x0F)
-    write_buf[0] = 0x0F;
-    dwm1001.LIS2DH12.write(address, &write_buf)
-        .expect("Failed to write to I2C");
-    dwm1001.LIS2DH12.read(address, &mut read_buf)
-        .expect("Failed to read from I2C");
-    assert_eq!(read_buf[0], 0b00110011);
+    let mut lis2dh12 =
+        lis2dh12::Lis2dh12::new(dwm1001.LIS2DH12, address).expect("lis2dh12 new failed");
 
-    print!("WHOAMI: {:08b}\n", read_buf[0]);
+    print!(
+        "WHOAMI: {:08b}\n",
+        lis2dh12
+            .get_device_id()
+            .expect("lis2dh12 get_device_id failed")
+    );
 
-    loop {}
+    lis2dh12
+        .set_mode(lis2dh12::Mode::HighResolution)
+        .expect("lis2dh12 set_mode failed");
+
+    lis2dh12
+        .set_odr(lis2dh12::Odr::Hz1)
+        .expect("lis2dh12 set_odr failed");
+
+    lis2dh12
+        .enable_axis((true, true, true))
+        .expect("lis2dh12 enable_axis failed");
+
+    lis2dh12
+        .enable_temp(true)
+        .expect("lis2dh2 enable_temp failed");
+
+    print!(
+        "TEMP: {:?}\n",
+        lis2dh12.get_temp_out().expect("lis2dh2 get_temp failed")
+    );
+
+    print!(
+        "TEMP_STATUS: {:?}\n",
+        lis2dh12
+            .get_temp_status()
+            .expect("lis2dh2 get_temp_status failed")
+    );
+
+    print!("STATUS: {:?}\n", lis2dh12.get_status());
+
+    loop {
+        print!("ACC: {:?}\n", lis2dh12.acceleration());
+    }
 }
